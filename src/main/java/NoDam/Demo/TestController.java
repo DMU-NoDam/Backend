850be78d4e312d28f45c4512b29012068d781f86
kwonhee1 <lkh8033@gmail.com>
2026-04-24 02:15:05 +0900

    feat (Test): feat Test Create User

--- src/main/java/NoDam/Demo/TestController.java
+++ src/main/java/NoDam/Demo/TestController.java
@@ -1,15 +1,30 @@
 package NoDam.Demo;
 
 import NoDam.Demo.common.SuccessResponse;
+import NoDam.Demo.common.excetion.CustomException;
+import NoDam.Demo.common.excetion.ErrorCode;
+import NoDam.Demo.user.domain.User;
+import NoDam.Demo.user.domain.UserRole;
+import NoDam.Demo.user.repository.UserRepository;
+import NoDam.Demo.user.service.JWTService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Map;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Tag(name = "testController")
+@RequiredArgsConstructor
 public class TestController {
+
+    private final UserRepository userRepository;
+    private final JWTService jwtService;
 
     @GetMapping("/test")
     public ResponseEntity ping() {
@@ -29,6 +44,41 @@ public class TestController {
     @GetMapping("/domain/admin")
     public ResponseEntity testAdmin() {
         return ResponseEntity.ok().body(new SuccessResponse("success", Map.of("ping", "pong")));
+    }
+
+    @PostMapping("/test/user")
+    public ResponseEntity createOrIssueTestUserToken(@RequestBody TestUserRequest request) {
+        User user;
+
+        if (request.getId() == null) {
+            user = userRepository.save(
+                    User.builder()
+                            .name("testUser")
+                            .role(UserRole.valueOf(request.getRole()))
+                            .oAuthId("testUser")
+                            .oAuthProvider("test")
+                            .build()
+            );
+        } else {
+            user = userRepository.findById(request.getId())
+                    .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+        }
+
+        return ResponseEntity.ok().body(new SuccessResponse(
+                "success",
+                Map.of(
+                        "userId",  user.getId(),
+                        "accessToken", jwtService.generateAccessToken(user.getId()),
+                        "refreshToken", jwtService.generateRefreshToken(user.getId())
+                )
+        ));
+    }
+
+    @Getter
+    @Setter
+    public static class TestUserRequest {
+        private Long id;
+        private String role;
     }
 
 }
