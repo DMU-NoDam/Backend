9c5f971ac3cc63105438ce955cae18877c6acc22
choelsol <joanna4065@naver.com>
2026-05-12 17:00:28 +0900

    fix: lon/lat column add

--- src/main/java/NoDam/Demo/region/domain/Region.java
+++ src/main/java/NoDam/Demo/region/domain/Region.java
@@ -12,12 +12,14 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "region")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Setter
 public class Region {
 
     @Id
@@ -30,14 +32,23 @@ public class Region {
     @Column(nullable = false, length = 5)
     private String code; // front를 위한 지역코드
 
+    @Column
+    private Double lat;
+
+    @Column
+    private Double lon;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "super_id", nullable = true)
     private Region superRegion; // 상위 region, null이면 최상위
 
     @Builder
-    public Region(String name, String code, Region superRegion) {
+    public Region(Long id, String name, String code, Double lat, Double lon, Region superRegion) {
+        this.id = id;
         this.name = name;
         this.code = code;
+        this.lat = lat;
+        this.lon = lon;
         this.superRegion = superRegion;
     }
 
