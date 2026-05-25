5b8811be5d0cb982f71c91183cd0c1375bcb995b
이권희 <lkh8033@gmail.com>
2026-05-25 13:33:11 +0900

    feat (common): fix place type return with korean

--- src/main/java/NoDam/Demo/common/type/PlaceType.java
+++ src/main/java/NoDam/Demo/common/type/PlaceType.java
@@ -2,10 +2,21 @@ package NoDam.Demo.common.type;
 
 public enum PlaceType {
 
-    RESTAURANT, // 식당
-    CAFE, // 카페 (추가됨 05/13)
-    SIGHT, // 관광지
-    SHOP, // 쇼핑
-    HOTEL,
-    AIRPORT // 공항
+    RESTAURANT("식당"), // 식당
+    CAFE("카페"), // 카페 (추가됨 05/13)
+    SIGHT("관광지"), // 관광지
+    SHOP("쇼핑"), // 쇼핑
+    HOTEL("호텔"),
+    AIRPORT("공항"); // 공항
+
+    private final String name;
+
+    PlaceType(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
