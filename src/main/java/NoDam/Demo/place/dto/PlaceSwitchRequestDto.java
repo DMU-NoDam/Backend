a2cdfe4b45613629ebbf4ce8ece6e039fbd0eca9
choelsol <joanna4065@naver.com>
2026-05-30 14:47:10 +0900

    feat (plan): feat swich place plan api

---
+++ src/main/java/NoDam/Demo/place/dto/PlaceSwitchRequestDto.java
@@ -0,0 +1,14 @@
+package NoDam.Demo.place.dto;
+
+import NoDam.Demo.common.type.PlaceType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PlaceSwitchRequestDto {
+
+    private Long placePlan1;
+    private Long placePlan2;
+
+}
